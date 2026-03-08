3ba199c507acf63a3a909ecc325a0cff98a8210b
alyzidan <Alyzidan.android@gmail.com>
2026-03-08 16:23:44 +0200

    config: read api from env variable

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
 export const PAGE_SIZE = 20;
-export const API_BASE = "https://pokeapi.co/api/v2";
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE ?? "https://pokeapi.co/api/v2";
 export const MAX_BASE_STAT = 255;
